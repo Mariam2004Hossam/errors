6b5324066849b305fe39ac554870be573900eed2
Mariam2004Hossam <mariamhossam1104@gmail.com>
2025-04-19 12:00:36 -0700

    Add files via upload

---
+++ Data/TechXpressDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechXpress.Models;
+
+namespace TechXpress.Data
+{
+    public class TechXpressDbContext : DbContext
+    {
+        public TechXpressDbContext(DbContextOptions<TechXpressDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        // Add other DbSets for different models
+    }
+}
