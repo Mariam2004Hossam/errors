8815c319dbb8f2a68b19546c3a46664d437d000b
Mariam2004Hossam <mariamhossam1104@gmail.com>
2025-04-19 12:02:28 -0700

    Add files via upload

---
+++ Models/ProductListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TechXpress.Models
+{
+    public class ProductListViewModel
+    {
+        public List<Product> Products { get; set; }
+        public string SearchString { get; set; }
+        public string Category { get; set; }
+    }
+}
